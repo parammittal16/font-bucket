aa1da51205314afa82fce6a2bd33b686b0b61f25
rishabhscross <parammittal16@gmail.com>
2019-12-01 11:00:18 +0530

    add setfonts function

--- fontbucket.js
+++ fontbucket.js
@@ -2,3 +2,13 @@ window.onload = getFonts();
 function getFonts(){
     console.log("Hello");
 };
+document.getElementById("addFontButton").onclick = setFonts;
+
+function setFonts() {
+    let url = document.getElementById('fontUrl').value;
+    console.log(url);
+    //
+    chrome.storage.sync.set({allFonts:[]}, function() {
+        console.log('Value is set to ' + value);
+    });
+}
